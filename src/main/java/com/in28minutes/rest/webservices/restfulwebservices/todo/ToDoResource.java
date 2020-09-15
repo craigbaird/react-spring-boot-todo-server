bad3bf78ad808ae1325af96b246c58c02f8d9797
Craig Baird <bairdcraig@gmail.com>
2020-09-15 08:13:46 -0500

    progress

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/ToDoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/ToDoResource.java
@@ -1,14 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.rest.webservices.restfulwebservices.todo.Todo;
 
@@ -39,4 +45,25 @@ public class ToDoResource {
 		
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PutMapping("/users/{username}/todos/{id}") 
+		public ResponseEntity<Todo> updateToDo(
+				@PathVariable String username, 
+				@PathVariable long id, @RequestBody Todo todo) {
+			Todo todoUpdate = todoService.save(todo);
+			return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+		}
+	
+	@PostMapping("/users/{username}/todos") 
+	public ResponseEntity<Void> updateToDo(
+			@PathVariable String username, @RequestBody Todo todo) {
+		Todo createdTodo = todoService.save(todo);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+
+	
 }
